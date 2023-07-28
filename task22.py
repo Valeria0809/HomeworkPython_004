d718998aded38e6e11fb5ecb5b9445bfd6fd6b25
VALERIA <valeri.tereshenkova@icloud.com>
2023-07-28 19:49:09 +0300

    Completed Task 22

--- task22.py
+++ task22.py
@@ -11,16 +11,15 @@ def create_array(n, mn, mx):
     array = [random.randint(mn, mx) for _ in range(n)]
     return array
 
+n = int(input('Кол-во элементов первого набора: '))
+m = int(input('Кол-во элементов второго набора: '))
+list_1 = create_array(n, 2, 13)
+list_2 = create_array(m, 2, 13)
+print(list_1)
+print(list_2)
 
-n = int(input('Кол-во элементов первого набора? -> '))
-m = int(input('Кол-во элементов второго набора? -> '))
-listing_1 = create_array(n, 2, 13)
-listing_2 = create_array(m, 2, 13)
-print(listing_1)
-print(listing_2)
-
-set_1 = set(listing_1)
-set_2 = set(listing_2)
+set_1 = set(list_1)
+set_2 = set(list_2)
 
 result = set_1 & set_2
 print('Пересечение множеств - ', sorted(result), '\n')
